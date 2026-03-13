47486664a365996e2a9d8d0c4afe700df631cd0b
Guillaume Jolibois <jolibois.contact@gmail.com>
2026-03-13 12:25:45 +0100

    Init react front & php symfony back

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue sur l'application React</h1>
+        <p>Frontend React connecté au backend Symfony</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
